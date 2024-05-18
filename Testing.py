1d656a4ff7e8f2b79f1aa988da5f738caa0b7193
Adi Suhit Jha <adi.suhit.jha@gmail.com>
2024-05-18 03:08:59 -0400

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -10,13 +10,14 @@ what_to_get = ['acc']
 
 start_time = time.time()
 data_dict = {}
-# gyro_dict = {}
-
+alreadymoving = False
+movementtimes = 0
+    
 def phyphox_data():
+    
     global data_dict
     response = r.get(url + '&'.join(what_to_get)).text
     data = json.loads(response)
-    current_time = time.time() - start_time
     
     # Extract acceleration and gyroscope data
     acc_data = data['buffer'][what_to_get[0]]['buffer'][0]
@@ -24,10 +25,9 @@ def phyphox_data():
     #apply high pass filter to data
     if acc_data < 0.1:
         acc_data = 0
-    
-    # Append data to dictionaries
-    data_dict[current_time] = acc_data
-    # gyro_dict[current_time] = gyro_data
+
+    print(acc_data)
+    return acc_data
     
     print(f'Time: {current_time:.2f}s, Acceleration: {acc_data}')
 def fastFourierTransform(time_acc_dict):
@@ -45,10 +45,23 @@ def fastFourierTransform(time_acc_dict):
     return frequencies, fft_result
 
 # Collect data 100 times with a 1 second interval
-for i in range(0, 20):
-    phyphox_data()
-    time.sleep(1)
+for i in range(0, 100):
+    if phyphox_data() < 0.1:
+        current_time = time.time() - start_time
+        data_dict[current_time] = phyphox_data()
+        if alreadymoving == True:
+            
+            alreadymoving = False
+    else:
+        if alreadymoving == False:
+            movementtimes += 1
+        
+        alreadymoving = True
+    
+    time.sleep(0.1)
     
 returned_frequencies, returned_fft_result = fastFourierTransform(data_dict)
 
 print(returned_frequencies)
+print(movementtimes)
+print((movementtimes/(time.time() - start_time)))
